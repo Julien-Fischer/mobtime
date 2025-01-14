72966d854e7f13cac2ecabc13f7f40fc67bfbe89
Julien Fischer <julien.fischer@agiledeveloper.net>
2025-01-14 13:35:34 +0100

    chore: Specify @Function interface on OnDone interface

--- src/main/java/net/agiledeveloper/mobtime/domain/ports/api/OnDone.java
+++ src/main/java/net/agiledeveloper/mobtime/domain/ports/api/OnDone.java
@@ -2,6 +2,7 @@ package net.agiledeveloper.mobtime.domain.ports.api;
 
 import net.agiledeveloper.mobtime.domain.session.Session;
 
+@FunctionalInterface
 public interface OnDone {
 
     void accept(Session session);
