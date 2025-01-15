0fc34eb8e87eb61a68562cdc203d4f3f0ab506f2
Julien Fischer <julien.fischer@agiledeveloper.net>
2025-01-15 19:31:00 +0100

    refactor: Parse input parameters using BashParameter

--- src/main/java/net/agiledeveloper/mobtime/infra/CommandLineParser.java
+++ src/main/java/net/agiledeveloper/mobtime/infra/CommandLineParser.java
@@ -10,7 +10,9 @@ import net.agiledeveloper.mobtime.domain.session.SessionService;
 import net.agiledeveloper.mobtime.utils.AppLogger;
 
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import static net.agiledeveloper.mobtime.utils.TimeConverter.minutesToSeconds;
@@ -30,27 +32,36 @@ public class CommandLineParser {
             throw new IllegalArgumentException("No command specified");
         }
 
+        var parameters = Arrays.stream(commandLine)
+                .map(BashParameter::new)
+                .toList();
+
+        return parseParameters(parameters);
+    }
+
+
+    private Command parseParameters(List<BashParameter> bashParameters) {
         Set<Parameter> parameters = new HashSet<>();
         Command command = null;
 
         AppLogger.logSeparator();
         AppLogger.log("Starting MobTime with parameters:");
-        for (var argument : commandLine) {
-            AppLogger.log(" ", argument);
+        for (var argument : bashParameters) {
+            AppLogger.log(" ", argument.toString());
 
-            if (argument.equals("--start")) {
+            if (argument.hasName("start")) {
                 command = new StartCommand(parameters, sessionService);
             }
 
-            else if (argument.equals("--dry-run")) {
+            else if (argument.hasName("dry-run")) {
                 parameters.add(new DryRunParameter());
             }
 
-            else if (argument.startsWith("--duration")) {
+            else if (argument.hasName("duration")) {
                 parameters.add(new DurationParameter(readDuration(argument)));
             }
 
-            else if (argument.startsWith("--invalid")) {
+            else if (argument.hasName("invalid")) {
                 var msg = "Error: --invalid is not a valid argument";
                 AppLogger.log(msg);
                 throw new IllegalArgumentException(msg);
@@ -65,12 +76,11 @@ public class CommandLineParser {
     }
 
 
-    private static Duration readDuration(String argument) {
-        String[] split = argument.split("=");
+    private static Duration readDuration(BashParameter argument) {
         var seconds = Session.DEFAULT_DURATION_SECONDS;
-        if (split.length == 2) {
+        if (argument.hasValue()) {
             try {
-                seconds = Integer.parseInt(split[1]);
+                seconds = Integer.parseInt(argument.value());
                 if (seconds < 0) {
                     throw new IllegalArgumentException("--duration can not be negative. Received: " + seconds);
                 }
@@ -81,8 +91,51 @@ public class CommandLineParser {
         return Duration.ofSeconds((long) minutesToSeconds(seconds));
     }
 
-    private static void reject(String argument, Throwable cause) {
-        throw new IllegalArgumentException("Invalid duration parameter: " + argument, cause);
+    private static void reject(BashParameter argument, Throwable cause) {
+        var message = "Invalid " + argument.name();
+        if (argument.hasValue()) {
+            message += " parameter: " + argument.value();
+        }
+        throw new IllegalArgumentException(message, cause);
+    }
+
+
+    private static final class BashParameter implements Parameter {
+
+        public static final String PREFIX = "--";
+        public static final String SEPARATOR = "=";
+
+        private final String[] split;
+
+        BashParameter(String argument) {
+            split = argument.substring(2).split(SEPARATOR);
+        }
+
+        @Override
+        public String name() {
+            return split[0];
+        }
+
+        public String value() {
+            return split[1];
+        }
+
+        public boolean hasName(String name) {
+            return split[0].equals(name);
+        }
+
+        public boolean hasValue() {
+            return split.length == 2;
+        }
+
+        @Override
+        public String toString() {
+            var subject = PREFIX + name();
+            if (hasValue()) {
+                subject += SEPARATOR + value();
+            }
+            return subject;
+        }
     }
 
 }
