592f90bfee83e4176578f2ad736b15b72970fbad
Julien Fischer <julien.fischer@agiledeveloper.net>
2025-01-15 19:17:05 +0100

    chore: Add Session#toString method

--- src/main/java/net/agiledeveloper/mobtime/domain/session/Session.java
+++ src/main/java/net/agiledeveloper/mobtime/domain/session/Session.java
@@ -3,6 +3,9 @@ package net.agiledeveloper.mobtime.domain.session;
 import java.time.Duration;
 import java.time.Instant;
 
+import static net.agiledeveloper.mobtime.utils.TimeFormatter.formatDuration;
+import static net.agiledeveloper.mobtime.utils.TimeFormatter.formatInstant;
+
 public record Session(Duration duration, Instant createdAt) {
 
     public static final double DEFAULT_DURATION_SECONDS = 15 * 60;
@@ -15,6 +18,13 @@ public record Session(Duration duration, Instant createdAt) {
 
     public Duration graceDuration() {
         return Duration.ofSeconds(DEFAULT_GRACE_DURATION_SECONDS);
+    }
+
+    @Override
+    public String toString() {
+        return "[Session]"
+                + " createdAt: " + formatInstant(createdAt) + ","
+                + " duration: " + formatDuration(duration);
     }
 
 }
