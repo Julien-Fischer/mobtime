d4347e0a2fe0349f5ca549d79f3188733b008edc
Julien Fischer <julien.fischer@agiledeveloper.net>
2025-01-17 20:16:48 +0100

    refactor: Make Palette a non-instantiable class

--- src/main/java/net/agiledeveloper/mobtime/infra/swing/Palette.java
+++ src/main/java/net/agiledeveloper/mobtime/infra/swing/Palette.java
@@ -2,14 +2,17 @@ package net.agiledeveloper.mobtime.infra.swing;
 
 import java.awt.*;
 
-public interface Palette {
+public class Palette {
 
-    Color WINDOW_BG = new Color(25, 25, 25);
-    Color BUTTON_BG = new Color(50, 50, 50);
-    Color BUTTON_FG = new Color(200, 200, 200);
+    public static final Color WINDOW_BG       = new Color(25, 25, 25);
+    public static final Color BUTTON_BG       = new Color(50, 50, 50);
+    public static final Color BUTTON_FG       = new Color(200, 200, 200);
 
-    Color MESSAGE_OK   = Color.GREEN;
-    Color MESSAGE_INFO = Color.YELLOW;
-    Color MESSAGE_WARN = Color.MAGENTA;
+    public static final Color MESSAGE_OK      = Color.GREEN;
+    public static final Color MESSAGE_INFO    = Color.YELLOW;
+    public static final Color MESSAGE_WARN    = Color.MAGENTA;
+    public static final Color MESSAGE_NEUTRAL = new Color(180, 180, 180);
+
+    private Palette() {}
 
 }
