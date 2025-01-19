c6c4ae7b4542a807aac015e31a3ea7518ce56c3b
Julien Fischer <julien.fischer@agiledeveloper.net>
2025-01-19 02:38:32 +0100

    feat: Make colors more visible

--- src/main/java/net/agiledeveloper/mobtime/infra/swing/theme/Palette.java
+++ src/main/java/net/agiledeveloper/mobtime/infra/swing/theme/Palette.java
@@ -6,12 +6,12 @@ public class Palette {
 
     public static final Color LIGHTGRAY    = new Color(200, 200, 200);
     public static final Color GRAY         = new Color(180, 180, 180);
-    public static final Color DARK_GRAY    = new Color(50, 50, 50);
+    public static final Color DARK_GRAY    = new Color(60, 60, 60);
     public static final Color DARKER_GRAY  = new Color(43, 45, 48);
 
     public static final Color GREEN        = new Color(87, 150, 92).brighter();
-    public static final Color YELLOW       = new Color(194, 158, 74);
-    public static final Color MAGENTA      = Color.MAGENTA;
+    public static final Color YELLOW       = new Color(194, 158, 74).brighter();
+    public static final Color MAGENTA      = new Color(255, 0, 255);
     public static final Color ORANGE       = new Color(207, 142, 109);
     public static final Color BLUE         = new Color(54, 117, 240);
 
