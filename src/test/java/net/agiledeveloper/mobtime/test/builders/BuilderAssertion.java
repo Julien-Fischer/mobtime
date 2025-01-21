3839e21bbeb2a5a452e35eea928ffbfc7825b0bc
Julien Fischer <julien.fischer@agiledeveloper.net>
2025-01-21 11:10:48 +0100

    test: Add builder assertions

---
+++ src/test/java/net/agiledeveloper/mobtime/test/builders/BuilderAssertion.java
@@ -0,0 +1,17 @@
+package net.agiledeveloper.mobtime.test.builders;
+
+import org.assertj.core.api.AbstractAssert;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BuilderAssertion {
+
+    public static <T> AbstractAssert<?, T> expectThat(T element) {
+        return assertThat(element);
+    }
+
+    public static <T> AbstractAssert<?, T> expectThat(Builder<T> builder) {
+        return assertThat(builder.build());
+    }
+
+}
